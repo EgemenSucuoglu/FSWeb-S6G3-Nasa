8c19b6fe20348211364f12abc51276053714f949
Egemen <sucuogluegemen1@gmail.com>
2023-03-29 13:51:57 +0300

    header yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import Header from "./Header";
 
 function App() {
+  const [date, setDate] = useState();
+  const [data, setData] = useState();
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=Dp1pZfSlbfwWM2WdkdyfpP5DxhxO1daYxwQZxrdo" +
+          date
+      )
+      .then((response) => setData(response.data));
+  }, [date]);
+
+  function dateChanger(date) {
+    let gun = new Date(date);
+    let gercekDate = `${gun.getFullYear()}-${gun.getMonth() +
+      1}-${gun.getDate()}`;
+    setDate(gercekDate);
+  }
   return (
     <div className="App">
+      <Header />
       <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
+        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip
+        edin İyi eğlenceler!{" "}
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
+        !
       </p>
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+function Header() {
+  return (
+    <header className="header">
+      <img
+        className="header--img"
+        src="https://www.nasa.gov/sites/default/files/images/174116main_2006_01777_highres.jpg"
+        alt="Nasa Logo"
+      />
+      <h1 className="header--logo">NASA LOGO</h1>
+    </header>
+  );
+}
+
+export default Header;
